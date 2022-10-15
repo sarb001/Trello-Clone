cfa42166364891d0576967214e9764070379690d
SARB <mrsinghbusiness05@gmail.com>
2022-10-15 16:16:50 +0530

    Button created

--- trello/src/App.js
+++ trello/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { IoIosAddCircle } from 'react-icons/io';
+
 
 function App() {
   return (
     <div className="App">
-      <h2>  Inside App </h2>
+      <h2>  Inside Appwdvcwd wrf we  </h2>
+       <span id = "btn-outside"> 
+        <IoIosAddCircle style = {{fontSize:'30px',color:'green'}}/> </span>
     </div>
   );
 }
